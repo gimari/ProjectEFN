08ba6c93c123cc233aa1ec4c10e7bb7306935f79
gimari <spldmg@gmail.com>
2021-03-23 00:15:38 +0900

    최초커밋

---
+++ Assets/Project/Scripts/Global/Global_Common.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Global_Common {
+
+	[Conditional("EFN_DEBUG")]
+	public static void Log(string str) {
+		UnityEngine.Debug.Log(str);
+	}
+
+	[Conditional("EFN_DEBUG")]
+	public static void LogError(string str) {
+		UnityEngine.Debug.LogError(str);
+	}
+
+	public static AsyncOperation LoadSceneAsync(string sceneName) {
+		return SceneManager.LoadSceneAsync(sceneName);
+	}
+
+	public static void LoadScene(string sceneName) {
+		SceneManager.LoadScene(sceneName);
+	}
+}
---
+++ Assets/Project/Scripts/Global/Global_DD.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global_DD : MonoBehaviour {
+	private void Awake() {
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
