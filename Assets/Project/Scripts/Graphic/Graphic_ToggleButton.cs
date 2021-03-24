369655565152d491ae06c30dbc81dd4cb77070cf
gimari <spldmg@gmail.com>
2021-03-25 02:11:25 +0900

    상인 이미지 추가, 버튼 애니메이션 추가

---
+++ Assets/Project/Scripts/Graphic/Graphic_ToggleButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Button))]
+public class Graphic_ToggleButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+	[SerializeField] private GameObject _targetObj = default;
+
+	private MaskableGraphic[] _graphicTargetList = null;
+
+	private void Awake() {
+		this._graphicTargetList = _targetObj.GetComponentsInChildren<MaskableGraphic>();
+	}
+	
+	public void OnPointerDown(PointerEventData eventData) {
+		_targetObj.SetActive(true);
+
+		foreach (MaskableGraphic graphic in this._graphicTargetList) {
+			graphic.DOFade(0, 0);
+			graphic.DOFade(1, 0.2f);
+		}
+	}
+
+	public void OnPointerUp(PointerEventData eventData) {
+		foreach (MaskableGraphic graphic in this._graphicTargetList) {
+			graphic.DOFade(0, 0.2f);
+		}
+	}
+
+}
